57fb6e95f9c6588c3318cd95d6e1c6fd8d1ee14f
Jawad Ali <jawadali5658@gmail.com>
2022-11-14 02:42:38 +0500

    new

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+  res.json("server started")
 });
 
 // Middleware for Errors
