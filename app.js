5f62ec34426d71c315dd34fcc549a14bb8635dc8
Jawad Ali <57399800+jawad-ali786@users.noreply.github.com>
2022-11-15 22:26:58 +0500

    cors fixing

--- app.js
+++ app.js
@@ -21,7 +21,11 @@ app.use(cors({
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // Route Imports
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
